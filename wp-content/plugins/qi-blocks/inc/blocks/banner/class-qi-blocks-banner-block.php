0273fa177b40b8cfd57ac2f747ab3fbd145097d4
Vlad <vlad@thesixbysix.co.uk>
2022-12-05 22:10:58 +0300

    ACF and QI blocks

---
+++ wp-content/plugins/qi-blocks/inc/blocks/banner/class-qi-blocks-banner-block.php
@@ -0,0 +1,34 @@
+<?php
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'Qi_Blocks_Banner_Block' ) ) {
+	class Qi_Blocks_Banner_Block extends Qi_Blocks_Blocks {
+		private static $instance;
+
+		public function __construct() {
+			// Set block data
+			$this->set_block_name( 'banner' );
+			$this->set_block_title( esc_html__( 'Banners', 'qi-blocks' ) );
+			$this->set_block_subcategory( esc_html__( 'Business', 'qi-blocks' ) );
+			$this->set_block_demo_url( 'https://qodeinteractive.com/qi-blocks-for-gutenberg/banners/' );
+			$this->set_block_documentation( 'https://qodeinteractive.com/qi-blocks-for-gutenberg/documentation/#banners' );
+
+			parent::__construct();
+		}
+
+		/**
+		 * @return Qi_Blocks_Banner_Block
+		 */
+		public static function get_instance() {
+			if ( is_null( self::$instance ) ) {
+				self::$instance = new self();
+			}
+
+			return self::$instance;
+		}
+	}
+
+	Qi_Blocks_Banner_Block::get_instance();
+}
